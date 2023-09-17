218b0f8e924a81510a3b53ed94c0c59d2c8e966f
dollar-src <78104813+dollar-src@users.noreply.github.com>
2023-09-17 19:41:19 +0300

    fixed dupe problem.

--- server.lua
+++ server.lua
@@ -1,49 +1,49 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-
 RegisterServerEvent('gunshop:serverbuyitem', function(data)
-    local bool =  false
+    local bool = false
     local a = source
     local b = QBCore.Functions.GetPlayer(a)
-    local c = json.encode(data.materials)
-    local materialItemS = 0
-    local materialItemAmount = 0
     local fn = b.PlayerData.charinfo.firstname
     local ln = b.PlayerData.charinfo.lastname
+    
+    local materialItemS = 0
+    local materialItemAmount = 0
+
     for k, v in pairs(Config.Recipes[data.item].materials) do       
-    --  print('Material Amount '.. v)
-    --  print('Item Name :' .. k)
         local materialItem = b.Functions.GetItemByName(k)
         materialItemS = materialItemS + 1
-        if  materialItem ~= nil  and  materialItem.amount >= v  then
+
+        if materialItem ~= nil and materialItem.amount >= v then
             materialItemAmount = materialItemAmount + 1
         end
-        if materialItemS == materialItemAmount then
-            bool = true
-        end
     end
+
+    if materialItemS == materialItemAmount then
+        bool = true
+    end
+
     if bool then
         for k, v in pairs(Config.Recipes[data.item].materials) do
-
-        b.Functions.RemoveItem(k, v)
-        TriggerClientEvent('inventory:client:ItemBox', a, QBCore.Shared.Items[k], "remove", v)
+            b.Functions.RemoveItem(k, v)
+            TriggerClientEvent('inventory:client:ItemBox', a, QBCore.Shared.Items[k], "remove", v)
         end
+
         Citizen.Wait(1000)
+
         if data.isWeapon then
             b.Functions.AddItem(data.item, 1)
             TriggerClientEvent('inventory:client:ItemBox', a, QBCore.Shared.Items[data.item], "add", 1)
-            discordlog(Config.SellWebhook, "Item Crafted","\n Firstname **: " ..fn.."**\n Lastname** : " ..ln.."** \n Player ID : **" ..a.."** \n Item : **"..data.item.."** \n Amount : **1**")
+            discordlog(Config.SellWebhook, "Item Crafted", "\n Firstname **: " .. fn .. "**\n Lastname** : " .. ln .. "** \n Player ID : **" .. a .. "** \n Item : **" .. data.item .. "** \n Amount : **1**")
         else
             b.Functions.AddItem(data.item, data.count)
             TriggerClientEvent('inventory:client:ItemBox', a, QBCore.Shared.Items[data.item], "add", data.count)
-            discordlog(Config.SellWebhook, "Item Crafted","\n Firstname **: " ..fn.."**\n Lastname** : " ..ln.."** \n Player ID : **" ..a.."** \n Item : **"..data.item.."** \n Amount : **"..data.count.."**")
-
+            discordlog(Config.SellWebhook, "Item Crafted", "\n Firstname **: " .. fn .. "**\n Lastname** : " .. ln .. "** \n Player ID : **" .. a .. "** \n Item : **" .. data.item .. "** \n Amount : **" .. data.count .. "**")
         end
-
     end
-
-
 end)
+
+
 
 function discordlog(webhook, title, text, target)
 	local ts = os.time()
